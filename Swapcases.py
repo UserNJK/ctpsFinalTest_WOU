35ce0a5d295bed595828db5497df0eab7308b25f
Jugal Kishore Nagulapalli <70318004+UserNJK@users.noreply.github.com>
2023-12-20 14:38:28 +0530

    Create Swapcases.py

---
+++ Swapcases.py
@@ -0,0 +1,9 @@
+'''
+1. You are given a string, and your task is to swap cases. In other words, convert all lowercase
+letters to uppercase letters and vice versa.
+'''
+
+inpstr1 = str(input())
+
+sol1 = inpstr1.swapcase()
+print(sol1)
